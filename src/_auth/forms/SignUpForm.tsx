e629c26f09f7b6f78ebf0736c9e8fc9c57276726
McBryan-dev <bryanokononfua@gmail.com>
2024-08-10 22:34:44 +0100

    Next Commit

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -1,5 +1,6 @@
-import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
+
+import { z } from 'zod';
 
 import {
   Form,
@@ -10,52 +11,126 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+
+import { SignupValidation } from '../../lib/validation/index.ts';
 import { Input } from "@/components/ui/input";
+import Loader from '@/components/shared/Loader';
 
  
 import { Button } from '../../components/ui/button';
 import { useForm } from 'react-hook-form';   
 
-const formSchema = z.object({
-   username: z.string().min(2).max(50),
- });
+
 
 
 //  SIGNUP FORM
  const SignUpForm = () => {
+
+  const isLoading = false;
   
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
     defaultValues: {
+      name: "",
       username: "",
+      email: "",
+      password: ""
     },
   })
   
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof SignupValidation>) {
     console.log(values)
   }
   
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
+
+      <div className="sm:w-420 flex-center flex-col">
+
+        <img src="/assets/images/logo.svg" alt="logo" />
+
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+          Create a new account
+        </h2>
+
+        <p className="text-light-3 small-medium md:base-regular mt-2">
+          To use Snapgram, please enter your account details
+        </p>
+
+      
+
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4 ">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>NAME</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                
+                <FormMessage /> 
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>USERNAME</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>EMAIL</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>PASSWORD</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="shad-button_primary">
+            {isLoading ? (
+              <div className="flex flex-centre gap-2">
+                <Loader />Loading...
+              </div>
+            ) : "Submit"}
+          </Button>
+
+              <p className="text-small-regular text-light-2 text-center mt-2"></p>
+
+        </form>
+      </div>
     </Form>
   )
 }
---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Loader = () => {
+    return (
+        <div className="flex flexcenter w-full">
+            <img 
+                src="/assets/icons/loader.svg" 
+                alt="Loader" 
+                width={24}
+                height={24}
+             />
+        </div>
+    )
+}
+
+export default Loader;
